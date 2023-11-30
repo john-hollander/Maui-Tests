32286acfc57845f3d58001ffb97c20651eb60118
John Hollander <john_hollander@hotmail.com>
2023-11-30 07:49:41 -0500

    Add project files.

---
+++ iOSFillTest/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace iOSFillTest
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ iOSFillTest/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace iOSFillTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnTestClicked(object sender, EventArgs e)
+        {
+            Column1.Width = new(1, GridUnitType.Star);
+            Column2.Width = new(0, GridUnitType.Star);
+        }
+    }
+
+}
