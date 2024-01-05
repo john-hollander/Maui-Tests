ba81d825543f8cbd768e9e3b690de6b14a70b9cc
John Hollander <john_hollander@hotmail.com>
2024-01-05 15:56:39 -0500

    Updates to DrawableTest.

--- IDrawableFontTest/DrawableTest.cs
+++ IDrawableFontTest/DrawableTest.cs
@@ -2,6 +2,13 @@
 {
     internal class DrawableTest : IDrawable
     {
+        private MainPage _page;
+
+        public DrawableTest(MainPage page)
+        {
+            _page = page;
+        }
+
         public void Draw(ICanvas canvas, RectF dirtyRect)
         {
             string fontFamily;
@@ -10,13 +17,22 @@
 
             RectF rect = dirtyRect.Inflate(-0.1f * dirtyRect.Width, -0.1f * dirtyRect.Height);
 
+            if (_page.Extent is not null)
+            {
+                if (_page.Extent.Value.X > rect.X && _page.Extent.Value.Y > rect.Y) 
+                {
+                    rect = new(rect.X, rect.Y, (float)_page.Extent.Value.X - rect.X, (float)_page.Extent.Value.Y - rect.Y);
+                    canvas.FillColor = Colors.Red;
+                    canvas.FillCircle(rect.Right, rect.Bottom, dirtyRect.Width / 120);
+                    canvas.FillColor = null;
+                }
+            }
+
             canvas.StrokeColor = Colors.Black;
             canvas.DrawRectangle(rect);
 
             Microsoft.Maui.Graphics.Font font;
             SizeF size;
-
-            // Get the size to display the text at 120 points.
 
             if (OperatingSystem.IsAndroid())
                 fontFamily = "Cinzel-Regular.ttf";
@@ -24,6 +40,9 @@
                 fontFamily = "Cinzel";
 
             font = new(fontFamily);
+            font = new("Arial");
+
+            // Get the size to display the text at 120 points.
             size = canvas.GetStringSize(text, font, 120f, HorizontalAlignment.Center, VerticalAlignment.Center);
 
             // Scale the font for use.
@@ -35,6 +54,8 @@
             RectF textRect = new(rect.Left + (rect.Width - size.Width) / 2,
                                  rect.Top + (rect.Height - size.Height) / 2,
                                  size.Width, size.Height);
+
+            canvas.FontSize = fontSize * 0.9921f;
 
             canvas.StrokeColor = Colors.Blue;
             canvas.DrawRectangle(textRect);
--- IDrawableFontTest/MainPage.xaml.cs
+++ IDrawableFontTest/MainPage.xaml.cs
@@ -1,13 +1,23 @@
-﻿namespace IDrawableFontTest
+﻿using System.Diagnostics;
+
+namespace IDrawableFontTest
 {
     public partial class MainPage : ContentPage
     {
+        public Point? Extent { get; set; }
+
         public MainPage()
         {
             InitializeComponent();
 
-            view.Drawable = new DrawableTest();
+            view.Drawable = new DrawableTest(this);
+        }
+
+        private void HandleTapped(object sender, TappedEventArgs e)
+        {
+            Extent = e.GetPosition(view);
+            view.Invalidate();
+            Debug.Print($"Pointer: {Extent}");
         }
     }
-
 }
