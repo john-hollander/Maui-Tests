a0e2eb6df5155ba6b3a31b86cb35636e9bf72e2b
John Hollander <john.hollander@stealthtrader.com>
2024-01-15 14:08:56 -0500

    Added Chess Fonts Test.

---
+++ ChessFontsTest/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ChessFontsTest
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
