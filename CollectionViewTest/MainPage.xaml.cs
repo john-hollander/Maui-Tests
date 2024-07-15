f4e01e15e256f2d8c3bea1a898ef11eb583d669d
John Hollander <john_hollander@hotmail.com>
2024-07-15 17:48:01 -0400

    Added basic collection view test that reproduces the backcolor issue.

---
+++ CollectionViewTest/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CollectionViewTest
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly List<RowItem> _rowItems =
+        [
+            new() {Background = Brush.Green, Text = "Row Number 1" },
+            new() {Background = Brush.Green, Text = "Row Number 2" },
+            new() {Background = Brush.Red, Text = "Row Number 3" },
+            new() {Background = Brush.Red, Text = "Row Number 4" },
+            new() {Background = Brush.Green, Text = "Row Number 5" },
+            new() {Background = Brush.Green, Text = "Row Number 6" },
+            new() {Background = Brush.Red, Text = "Row Number 7" },
+            new() {Background = Brush.Red, Text = "Row Number 8" },
+            new() {Background = Brush.Green, Text = "Row Number 9" },
+            new() {Background = Brush.Green, Text = "Row Number 10" },
+        ];
+
+        internal ObservableCollection<RowItem> ListItems { get; } = [];
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Can't do with BindingContext because of nesting.
+            testList.ItemsSource = ListItems;
+        }
+
+        private void HandleTestClicked(object sender, EventArgs e)
+        {
+            ListItems.Clear();
+
+            for (int index = 0; index < _rowItems.Count;  index++)
+                ListItems.Add(_rowItems[index]);
+        }
+    }
+}
---
+++ CollectionViewTest/RowItem.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+
+namespace CollectionViewTest
+{
+    public class RowItem : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged = delegate { };
+
+        private Brush _background = Brush.Yellow;
+        private string _text = string.Empty;
+
+        public string Text
+        {
+            get => _text; 
+            set
+            {
+                if (_text != value)
+                {
+                    _text = value;
+                    RaisePropertyChanged(nameof(Text));
+                }
+            }
+        }
+
+        public Brush Background
+        {
+            get => _background;
+            set
+            {
+                if (_background != value)
+                {
+                    _background = value;
+                    RaisePropertyChanged(nameof(Background));
+                }
+            }
+        }
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
